6ef63859c13a3fbc5010984eb416fb004f96197b
DongHeon Lee <niche3600@gmail.com>
2022-12-13 16:39:06 +0900

    README更新

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const base = './'
+// index.htmlをsrc配下においたことによる設定
 const root = 'src'
+// rootを基準に参照するpublicパスを指定
 const publicDir = '../public'
 const build = {
+  // rootを基準に参照するビルドパスを指定
   outDir: '../dist',
+  // プロジェクトルートの外に出力先を指定する際、ビルド実行前にビルドディレクトリを空にする
   emptyOutDir: true,
 }
 // 起動したら自動でブラウザを開く
