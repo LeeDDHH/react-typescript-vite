0addeb966e627c1e9f790e5acd1cc6d1efb0a333
DongHeon Lee <niche3600@gmail.com>
2022-12-13 11:40:57 +0900

    起動したら自動でブラウザを開く

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 起動したら自動でブラウザを開く
+  server: {
+    open: true,
+  },
   plugins: [react()],
 })
