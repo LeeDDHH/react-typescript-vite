cd9bf2482e379b177c47d31b99b5cf8f09ff6bd1
DongHeon Lee <niche3600@gmail.com>
2022-12-13 15:53:07 +0900

    src配下にindex.htmlを配置、viteの設定を修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = './'
+const root = 'src'
+const publicDir = '../public'
+const build = {
+  outDir: '../dist',
+  emptyOutDir: true,
+}
+// 起動したら自動でブラウザを開く
+const server = { open: true }
+const plugins = [react()]
+
+const settings = { base, root, publicDir, build, server, plugins }
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  // 起動したら自動でブラウザを開く
-  server: {
-    open: true,
-  },
-  plugins: [react()],
-})
+export default defineConfig(settings)
