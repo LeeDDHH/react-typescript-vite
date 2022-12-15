87887fd0cd6b1b92c9af42714dfd73aa65de041a
DongHeon Lee <niche3600@gmail.com>
2022-12-15 23:29:24 +0900

    web-vitals導入

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
 import './index.css'
+import reportWebVitals from './lib/reportWebVitals'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+reportWebVitals(console.log)
